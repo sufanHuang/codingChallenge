9342d9f24b2b69498431a3d915c365751a6ea25e
sufanHuang <sufanhk@gmail.com>
2020-01-11 13:45:25 -0600

    Modify for deployment.

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ const app = express()
 const port = 3000
 
 app.use(express.static('./static'))
+app.get('/', (req,res)=>{
+    res.sendFile(__dirname + '/static/index.html')
+})
 
 app.listen(port, ()=>{
     console.log('Server is running on port:' + port)
