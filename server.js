1cade54e0db24f3ddde37cb17b70469ffd21e436
sufanHuang <sufanhk@gmail.com>
2020-01-11 15:23:18 -0600

    Final.

--- server.js
+++ server.js
@@ -3,9 +3,7 @@ const app = express()
 const port = 3000
 
 app.use(express.static('./static'))
-app.get('/', (req,res)=>{
-    res.sendFile(__dirname + '/static/index.html')
-})
+
 
 app.listen(port, ()=>{
     console.log('Server is running on port:' + port)
